58e08ff5e708b12b30f8745b1cb5b14d7c0001a3
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-10-31 22:00:14 +0900

    refactor: existsScreeningTime을 jpa를 사용하도록 개선

--- backend/src/main/java/com/rejs/reservation/domain/screening/service/ScreeningService.java
+++ backend/src/main/java/com/rejs/reservation/domain/screening/service/ScreeningService.java
@@ -5,11 +5,9 @@ import com.rejs.reservation.domain.movie.repository.MovieRepository;
 import com.rejs.reservation.domain.screening.dto.ScreeningDto;
 import com.rejs.reservation.domain.screening.dto.request.CreateScreeningRequest;
 import com.rejs.reservation.domain.screening.entity.Screening;
-import com.rejs.reservation.domain.screening.repository.ScreeningQueryRepository;
 import com.rejs.reservation.domain.screening.repository.ScreeningRepository;
 import com.rejs.reservation.domain.theater.entity.Theater;
 import com.rejs.reservation.domain.theater.repository.TheaterRepository;
-import com.rejs.reservation.domain.theater.service.TheaterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,7 +16,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class ScreeningService {
     private final ScreeningRepository screeningRepository;
-    private final ScreeningQueryRepository screeningQueryRepository;
     private final MovieRepository movieRepository;
     private final TheaterRepository theaterRepository;
 
@@ -29,7 +26,7 @@ public class ScreeningService {
         Movie movie = movieRepository.findById(request.getMovieId()).orElseThrow();
         Theater theater = theaterRepository.findById(request.getTheaterId()).orElseThrow();
         Screening screening = Screening.of(request.getStartTime(), theater, movie);
-        boolean isExists = screeningQueryRepository.existsByScreeningTime(screening.getTheaterId(), screening.getStartTime(), screening.getEndTime());
+        boolean isExists = screeningRepository.existsByScreeningTime(screening.getTheaterId(), screening.getStartTime(), screening.getEndTime());
 
         if(isExists){
             throw new RuntimeException();
