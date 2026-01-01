caf8334bb14a344c8454a0b2402a515e3dbfc4e2
Lee jong sik <102013392+js990311@users.noreply.github.com>
2026-01-01 17:52:09 +0900

    fix: 결제 취소 scheduler가 작동하지 않는 부분에 대해서 처리

--- backend/src/main/java/com/rejs/reservation/domain/reservation/repository/AutoCancelRepository.java
+++ backend/src/main/java/com/rejs/reservation/domain/reservation/repository/AutoCancelRepository.java
@@ -7,7 +7,10 @@ import com.rejs.reservation.domain.payments.entity.payment.QPayment;
 import com.rejs.reservation.domain.reservation.entity.QReservation;
 import com.rejs.reservation.domain.reservation.entity.QReservationSeat;
 import com.rejs.reservation.domain.reservation.entity.ReservationStatus;
+import com.rejs.reservation.domain.reservation.repository.jpa.ReservationRepository;
 import com.rejs.reservation.domain.screening.entity.QScreening;
+import com.rejs.reservation.domain.screening.entity.QScreeningSeat;
+import com.rejs.reservation.domain.screening.entity.ScreeningSeatStatus;
 import com.rejs.reservation.domain.theater.entity.QSeat;
 import com.rejs.reservation.domain.theater.entity.QTheater;
 import lombok.RequiredArgsConstructor;
@@ -15,12 +18,14 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Transactional
 @Repository
 @RequiredArgsConstructor
 public class AutoCancelRepository {
     private final JPAQueryFactory jpaQueryFactory;
+    private final ReservationRepository reservationRepository;
 
     private QReservation reservation = QReservation.reservation;
     private QReservationSeat reservationSeat = QReservationSeat.reservationSeat;
@@ -29,6 +34,7 @@ public class AutoCancelRepository {
     private QTheater theater = QTheater.theater;
     private QSeat seat = QSeat.seat;
     private QPayment payment = QPayment.payment;
+    private QScreeningSeat screeningSeat = QScreeningSeat.screeningSeat;
 
 
     public long autoCancelByCreatedAt(){
@@ -56,4 +62,34 @@ public class AutoCancelRepository {
                 ).execute();
     }
 
+    public long autoCancel(int limit){
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime threshold = now.minusMinutes(10);
+
+        List<Long> targets = reservationRepository.autoCancelReservation(now, threshold, limit);
+
+        if(targets.isEmpty()){
+            return 0;
+        }
+
+        jpaQueryFactory
+                .update(reservation)
+                .set(reservation.status, ReservationStatus.CANCELED)
+                .where(reservation.id.in(targets)).execute();
+
+        List<Long> screeningSeatIds = jpaQueryFactory
+                .select(reservationSeat.screeningSeat.id)
+                .from(reservationSeat)
+                .where(reservationSeat.reservation.id.in(targets))
+                .fetch();
+
+        jpaQueryFactory
+                .update(screeningSeat)
+                .set(screeningSeat.status, ScreeningSeatStatus.AVAILABLE)
+                .where(screeningSeat.id.in(
+                        screeningSeatIds
+                ))
+                .execute();
+        return targets.size();
+    }
 }
--- backend/src/main/java/com/rejs/reservation/domain/reservation/scheduler/ReservationCancelScheduler.java
+++ backend/src/main/java/com/rejs/reservation/domain/reservation/scheduler/ReservationCancelScheduler.java
@@ -1,13 +1,11 @@
 package com.rejs.reservation.domain.reservation.scheduler;
 
 import com.rejs.reservation.domain.reservation.repository.AutoCancelRepository;
-import com.rejs.reservation.domain.reservation.repository.ReservationDataFacade;
-import com.rejs.reservation.domain.reservation.service.ReservationService;
+import io.micrometer.observation.annotation.Observed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -15,10 +13,9 @@ import org.springframework.stereotype.Service;
 public class ReservationCancelScheduler {
     private final AutoCancelRepository autoCancelRepository;
 
-    @Scheduled(fixedDelay = 60*1000) // 1분마다 실행
+    @Observed
+    @Scheduled(fixedDelay = 20*1000)
     public void autoCancelReservation(){
-        // 트랜잭션은 repository에서 각각 실행됨
-        autoCancelRepository.autoCancelByCreatedAt();
-        autoCancelRepository.autoCancelByScreeningStartTime();
+        long target = autoCancelRepository.autoCancel(200);
     }
 }
