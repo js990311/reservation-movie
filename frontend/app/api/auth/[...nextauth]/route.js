000ea5ee4c7b77a724e5d04ea34acce1cbcdaa58
Lee jong sik <102013392+js990311@users.noreply.github.com>
2026-01-19 18:36:35 +0900

    feat: jwt 콜백

--- frontend/app/api/auth/[...nextauth]/route.js
+++ frontend/app/api/auth/[...nextauth]/route.js
@@ -20,14 +20,37 @@ export const authOptions = {
                     headers:{"Content-Type":"application/json"}
                 });
 
-                const user = await response.json();
-                if(response.ok && user){
-                    return user;
+                const loginResponse = await response.json();
+                if(response.ok && loginResponse.data){
+                    return loginResponse.data;
                 }
                 return null;
-            }
+            },
         })
-    ]
+    ],
+    callbacks: {
+        async jwt({token, user}){
+            console.log(token);
+            console.log(user);
+            // next-auth 내부의 정보
+            if(user){
+                token.name = user.email;
+                token.sub = user.email;
+                token.email = user.email;
+                token.role = user.role;
+                token.accessToken= user.tokens.accessToken.token;
+                token.refreshToken= user.tokens.refreshToken.token;
+                token.accessTokenExpire = user.tokens.accessToken.expiresAt;
+                token.refreshTokenExpire = user.tokens.refreshToken.expiresAt;
+            }
+            return token;
+        },
+        async session({session, token}){
+            // 브라우저에서 접근가능한 객체
+            console.log("session");
+            console.log(token);
+        }
+    }
 }
 
 const handler = NextAuth(authOptions);
